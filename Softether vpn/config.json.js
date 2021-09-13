841966391a2718d2ed41d256110f54cc6bacf167
tinycellstuan <89169192+tinycellstuan@users.noreply.github.com>
2021-09-13 17:42:22 -0500

    Create config.json.js

---
+++ Softether vpn/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"1.0.1",
+"md5":"ca50a5ed0db3087cc44b5d4e9fd75e4b",
+"home_url":"Module_softether_vpn.asp",
+"title":"softether_vpn",
+"description":"softether_vpn",
+"build_date":"2017-03-22_19:24:37"
+}
